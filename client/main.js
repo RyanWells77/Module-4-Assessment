551c56a23202579cf2989b4adde04cafdafc56bb
Ryan Wells <rswells2@gmail.com>
2024-02-17 20:57:01 -0700

    starwarsshipsbtn is comunicating correctly

--- client/main.js
+++ client/main.js
@@ -5,7 +5,8 @@ const starShipSubmitBtn = document.getElementById("starShip-button")
 const starShipList = document.getElementById("Ship-list")
 const starShipElement = document.createElement("div")
 const shipForm = document.getElementById("shipForm")
-const shipList = document.getElementById("shipList")
+// const shipList = document.getElementById("shipList")
+const baseURL = ("http://localhost:4000/api")
 
 const deleteShip = id => axios.delete(`${baseURL}/${id}`).then().catch(errCallback)
 
@@ -19,35 +20,55 @@ const displayStarships = (starships) => {
 
         shipNameElement.textContent = starship.name
         shipNameElement.addEventListener('click', () => {
-            shipDetails(starship.url, newStarShipElement)
+            shipDetails(`${baseURL}/getShips`, newStarShipElement)
         })
         newStarShipElement.appendChild(shipNameElement)
         starShipList.appendChild(newStarShipElement)
     })
 }
 
+const shipList = () => {
+    axios.get(`${baseURL}/getShips`)
+        .then(response => {
+            console.log("response.data:", response.data)
+            const ships = response.data
+            console.log("ships:", ships)
+            ships.forEach(starShip => {
+                const shipNameElement = document.createElement("h2")
+                const newStarShipElement = document.createElement("div")
 
-const shipDetails = (url, starShipElement) => {
-    axios.get(url)
-    .then(res => {
-        const shipDetails = res.data
-        const moreShipDetails = document.createElement("div")
-        const shipAdditonalDetailsLs = document.createElement("ul")
-        
-        for(const [key, value] of Object.entries(shipDetails)){
-            const detailItems = document.createElement("li")
-            detailItems.textContent = `${key}: ${value}`
-            shipAdditonalDetailsLs.appendChild(detailItems)
-        }
-        moreShipDetails.appendChild(shipAdditonalDetailsLs)
-        starShipElement.appendChild(moreShipDetails)
-    })
-    .catch(error => {
-        console.log("Error cant getting details", error)
-    })
+                shipNameElement.textContent = starShip.name
+                shipNameElement.addEventListener('click', () => {
+                    shipDetails(`${baseURL}/getShips/${starShip.name}`, newStarShipElement)
+                })
+                newStarShipElement.appendChild(shipNameElement)
+                starShipList.appendChild(newStarShipElement)
+            })
+        })
 }
 
-const addShip = (event) =>{
+
+const shipDetails = (url, starShipElement) => {
+    axios.get()
+        .then(res => {
+            const shipDetails = res.data
+            const moreShipDetails = document.createElement("div")
+            const shipAdditonalDetailsLs = document.createElement("ul")
+
+            for (const [key, value] of Object.entries(shipDetails)) {
+                const detailItems = document.createElement("li")
+                detailItems.textContent = `${key}: ${value}`
+                shipAdditonalDetailsLs.appendChild(detailItems)
+            }
+            moreShipDetails.appendChild(shipAdditonalDetailsLs)
+            starShipElement.appendChild(moreShipDetails)
+        })
+        .catch(error => {
+            console.log("Error cant getting details", error)
+        })
+}
+
+const addShip = (event) => {
     event.preventDefault()
 
     const formData = new FormData(shipForm)
@@ -61,30 +82,31 @@ const addShip = (event) =>{
 
 const getCompliment = () => {
     axios.get("http://localhost:4000/api/compliment/")
-    .then(res => {
-        const data = res.data;
-        alert(data);
-    });
+        .then(res => {
+            const data = res.data;
+            alert(data);
+        });
 };
 
 const getFortune = () => {
     axios.get("http://localhost:4000/api/fortune")
-    .then(res => {
-        const data = res.data
-        alert(data)
-    })
+        .then(res => {
+            const data = res.data
+            alert(data)
+        })
 }
 
 shipForm.addEventListener("submit", addShip)
-starWarsShipsBtn.addEventListener("click", () => {
-    axios.get("https://swapi.dev/api/starships/")
-        .then(res => {
-            const starships = res.data.results;
-            displayStarships(starships);
-        })
-        .catch(error => {
-            console.log("Error fetching ships", error)
-        })
-})
+starWarsShipsBtn.addEventListener("click", shipList)
+// starWarsShipsBtn.addEventListener("click", () => {
+//     axios.get(`${baseURL}/getShips`)
+//         .then(res => {
+//             const starships = res.data.results;
+//             displayStarships(starships);
+//         })
+//         .catch(error => {
+//             console.log("Error fetching ships.", error)
+//         })
+// })
 fortuneBtn.addEventListener("click", getFortune)
 complimentBtn.addEventListener('click', getCompliment)
--- server/controller.js
+++ server/controller.js
@@ -1,12 +1,14 @@
+const axios = require('axios')
+
 module.exports = {
 
     getCompliment: (req, res) => {
         const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
-      
+
         // choose random compliment
         let randomIndex = Math.floor(Math.random() * compliments.length);
         let randomCompliment = compliments[randomIndex];
-      
+
         res.status(200).send(randomCompliment);
     },
 
@@ -21,45 +23,96 @@ module.exports = {
 
     getShips: (req, res) => {
         axios.get("https://swapi.dev/api/starships/")
-        .then(response => {
-            const shipsList = response.data.results.map(ship => ({
-                name: ship.name
-            }))
-            res.status(200).send("Getting Ships.")
-        })
+            .then(response => {
+                // console.log("response", response.data)
+                const shipsList = response.data.results.map(ship => {
+                    const { name,
+                        model,
+                        manufacturer,
+                        cost_in_credits,
+                        length,
+                        max_atmosphering_speed,
+                        crew, passengers,
+                        cargo_capacity,
+                        consumables,
+                        hyperdrive_rating,
+                        MGLT,
+                        starship_class
+                    } = ship
+                    // console.log(ship)
+                    return {
+                        name,
+                        model,
+                        manufacturer,
+                        cost_in_credits,
+                        length,
+                        max_atmosphering_speed,
+                        crew,
+                        passengers,
+                        cargo_capacity,
+                        consumables,
+                        hyperdrive_rating,
+                        MGLT,
+                        starship_class
+                    }
+                })
+                res.status(200).json(shipsList)
+    })
         .catch(error => {
-            res.status(500).send("Failed to get Ships.")
-        })
+        res.status(500).send("Failed to get Ships.")
+    })
 
-    },
-    
-    addShip:  (req, res) => {
-        const { name, model, manufacturer, cost_in_credits, length } = req.body;
-        const newShip = {
-            name: body.name,
-            model: body.model,
-            manufacturer: body.manufacturer,
-            cost_in_credits: cost_in_credits,
-            }
-    
-        console.log("New ship added:");
-        console.log("Name:", name);
-        console.log("Model:", model);
-        console.log("Manufacturer:", manufacturer);
-        console.log("Cost in credits:", cost_in_credits);
-        console.log("Length:", length);
-    
-        res.status(200).send("")
-    },
+},
+
+//     shipDetails: (req, res) => {
+//         axios.get("https://swapi.dev/api/starships/")
+//             .then(response => {
+//                 const shipDetails = response.data.results.map(ship => ({ name, model, manufacturer, cost_in_credits, length } = response.data.results.map
+//                 const newShip = {
+//                     name: body.name,
+//                     model: body.model,
+//                     manufacturer: body.manufacturer,
+//                     cost_in_credits: cost_in_credits,
+//                 }))
+//         res.status(200).send("Getting Ships.")
+//     })
+//         .catch (error => {
+//     res.status(500).send("Failed to get Ships.")
+// })
+
+//     },
+
+// shipDetails: (req, res) => {
+//     const shipName = req.params.shipName
+// }
+
+addShip: (req, res) => {
+    const { name, model, manufacturer, cost_in_credits, length } = req.body;
+    const newShip = {
+        name: body.name,
+        model: body.model,
+        manufacturer: body.manufacturer,
+        cost_in_credits: cost_in_credits,
+    }
+
+    console.log("New ship added:");
+    console.log("Name:", name);
+    console.log("Model:", model);
+    console.log("Manufacturer:", manufacturer);
+    console.log("Cost in credits:", cost_in_credits);
+    console.log("Length:", length);
+
+    res.status(200).send("")
+},
 
     deleteShip: (req, res) => {
-        const {id} = req.params
+        const { id } = req.params
 
         const index = ships.findIndex(ships => {
             console.log(ships, id)
             return ships.id === +id
         })
-        if (ships[index] > globalId){
+        if (ships[index] > globalId) {
             res.status(400).send("id is not in data base.")
         }
         ships.splice(index, 1)
